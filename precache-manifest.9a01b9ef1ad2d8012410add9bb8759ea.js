ce36fd19447eba7ae64a06a6f205cde665936f62
kaveendra96 <kaveendra96@gmail.com>
2022-12-25 10:03:36 +0530

    Updates

---
+++ precache-manifest.9a01b9ef1ad2d8012410add9bb8759ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0423f6b8a6b6ca30856f775a916eef",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "5baa5dc32691231cb2e0",
+    "url": "/image-search/static/css/main.091c5285.chunk.css"
+  },
+  {
+    "revision": "607f80ab181bd5363674",
+    "url": "/image-search/static/js/2.4fc5202e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-search/static/js/2.4fc5202e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5baa5dc32691231cb2e0",
+    "url": "/image-search/static/js/main.5914f934.chunk.js"
+  },
+  {
+    "revision": "5cd995e2e94a943a249e",
+    "url": "/image-search/static/js/runtime-main.3dd0bf20.js"
+  }
+]);
